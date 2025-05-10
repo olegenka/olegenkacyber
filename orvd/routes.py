e6f909471abb3d5cc89f0c295bd4120e49b80a3e
kill Bill <60369904+olegenka@users.noreply.github.com>
2025-05-11 00:33:25 +0300

    Update routes.py

--- orvd/routes.py
+++ orvd/routes.py
@@ -1559,10 +1559,8 @@ def export_forbidden_zones():
     token = request.args.get('token')
     if token is None or not check_user_token(token):
         return jsonify({"error": "Unauthorized"}), 401
-
-    return send_file(FORBIDDEN_ZONES_PATH, as_attachment=True, attachment_filename='forbidden_zones.json')
-
-
+    return send_file(FORBIDDEN_ZONES_PATH, as_attachment=True, download_name='forbidden_zones.json')
+    
 @bp.route('/admin/import_forbidden_zones', methods=['POST'])
 def import_forbidden_zones():
     """
