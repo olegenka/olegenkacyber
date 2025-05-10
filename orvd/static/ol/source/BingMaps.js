253de2dc3043c91932528626f87b29924a1f9364
kill Bill <60369904+olegenka@users.noreply.github.com>
2025-05-11 01:15:04 +0300

    Update BingMaps.js

--- orvd/static/ol/source/BingMaps.js
+++ orvd/static/ol/source/BingMaps.js
@@ -181,11 +181,18 @@ class BingMaps extends TileImage {
       '&c=' +
       this.culture_;
 
+    this.loadMetadata_(url);
+  }
+
+  /**
+   * @private
+   * @param {string} url URL для загрузки метаданных.
+   */
+  loadMetadata_(url) {
     fetch(url)
       .then((response) => response.json())
       .then((json) => this.handleImageryMetadataResponse(json));
   }
-
   /**
    * Get the api key used for this source.
    *
